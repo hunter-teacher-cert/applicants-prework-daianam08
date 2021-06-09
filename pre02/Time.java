d21bfda54d3411b1387d00e4db83c3b10ca291e8
daiana munoz <daiana.m.0708@gmail.com>
2021-06-09 08:50:13 -0400

    finished?

--- pre02/Time.java
+++ pre02/Time.java
@@ -3,16 +3,19 @@ import java.util.*;
 
 public class Time{
 	public static void main (String [] args){
-	int hour= 3;
-	int min= 27;
-	int seconds= 45;
-	int timeSinceMidnight= (hour * 60)+(min*60)+seconds;
+	double hour= 3;
+	double min= 27;
+	double seconds= 45;
+	double timeSinceMidnight= (hour * 60)+(min*60)+seconds;
 	System.out.println (timeSinceMidnight + "seconds since midnight");
-	int timeUntilMidnight= (24*60*60) - timeSinceMidnight;
+	double timeUntilMidnight= (24*60*60) - timeSinceMidnight;
 	System.out.println(timeUntilMidnight+ "seconds until midnight");
 	double percentTimePassed= (timeSinceMidnight/timeUntilMidnight);
-	System.out.println(percentTimePassed + "percentage of time passed?");
-	//review double and percentage stuff
+	System.out.println(percentTimePassed + "percentage of time passed");
+
+	double timeNow= (8*3600)+(47*60)+36
+	System.out.println("About " + (timeNow-timeSinceMidnight) "has passed since I started this lab") 
+
 	
 	}
 }
