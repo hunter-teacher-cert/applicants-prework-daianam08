05c5428d96162cee7c4de5c8f335c8fc121082b0
daiana munoz <daiana.m.0708@gmail.com>
2021-06-09 03:43:29 -0400

    updated assignment, almost done with chpt 2

--- pre02/Time.java
+++ pre02/Time.java
@@ -2,16 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Time{
-	public status void main (String [] args){
+	public static void main (String [] args){
 	int hour= 3;
 	int min= 27;
 	int seconds= 45;
-	int timeSinceMidnight= (hour * 120)+(min*60)+seconds
-	System.out.println (timeSinceMidnight + "seconds since midnight")
-	int timeUntilMidnight= timeSinceMidnight - (24*120)
-	System.out.println(timeUntilMidnight)
-	double percentTimePassed= (timeSinceMidnight/timeUntilMidnight*100)
-	//finished step 4, just add println
+	int timeSinceMidnight= (hour * 60)+(min*60)+seconds;
+	System.out.println (timeSinceMidnight + "seconds since midnight");
+	int timeUntilMidnight= (24*60*60) - timeSinceMidnight;
+	System.out.println(timeUntilMidnight+ "seconds until midnight");
+	double percentTimePassed= (timeSinceMidnight/timeUntilMidnight);
+	System.out.println(percentTimePassed + "percentage of time passed?");
+	//review double and percentage stuff
 	
 	}
 }
