d4a05e45c752e0fd92d730f135a06e7f241c7004
daiana munoz <daiana.m.0708@gmail.com>
2021-06-09 03:15:00 -0400

    folder 2

--- pre02/Date.java
+++ pre02/Date.java
@@ -1,6 +1,20 @@
-String month= June;
-String day= Tuesday;
-int date= 8;
-int year= 2021;
+import java.io.*;
+import java.util.*;
 
-println(month, day, date, year);
+public class Date { 
+    public static void main(String[] args) {
+String day;
+day= "Tuesday";
+int date;
+date = 8;
+String month;
+month = "June";
+int year;
+year = 2021;
+System.out.print("American format:");
+System.out.print(day, month + date, year);
+System.out.print("European format:");
+System.out.println(day, date + month,year);
+
+	}
+}
