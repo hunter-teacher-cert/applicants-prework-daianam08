b12c387716b26094c82e2ce155e04ae2cb074e8d
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-06-28 02:44:36 +0000

    still working on craps :/

--- pre08/Craps.java
+++ pre08/Craps.java
@@ -3,26 +3,53 @@ import java.util.*;
 
 public class Craps{
   public static void main(String[]args){
-    shoot(3);
+    // part 1
+    Scanner inr = new Scanner(System.in);
+    System.out.println("To roll, gimme a number: ");
+    int rollNum= inr.nextInt();
+    System.out.println("You rolled: "+ roll(rollNum));
+
+    // part 2
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter number of dice: ");
+    int dice= in.nextInt();
+    System.out.println("Highest possible roll with entered dice number: " + shoot(dice));
+
+    //part 3
+    int [] game= {roll(rollNum), shoot(dice)};
+
 
   } //main
 
   
-    // public static int roll(a){
-    //   Random rollNum= new Random();
-    //   int numb= random.nextInt(a)+1;
-    // }
+  public static int roll(int rolled){
+    Random roller= new Random();
+    int numb= roller.nextInt(rolled)+1;
+    return numb;
+    } //roll
 
   public static int shoot(int dice){
     final int MAXDICE= 6;
-    Scanner in = new Scanner(System.in);
-    System.out.println("Enter number of dice: ");
-    dice= in.nextInt();
     int maxDice= dice * MAXDICE;
-    System.out.printf("Max number from roll= %1f", maxDice);
-  }
+    return maxDice;
+    } //shoot
 
-
+  public static int round(){
+    int [] lose={2, 3, 12};
+    int [] win= {7,11};
+    for (int i=0; i<lose.length; i++){
+    if (lose[i]){
+      Sytem.out.println("Craps! You lose!");
+    } else if (win[i]){
+      System.out.println("Natural! You win!");
+    } else{
+      System.out.println("Your value is a Point!");
+      if (win[i]==7){
+        Sytem.out.println("Craps! You lose!");
+      }
+    }
+    }
+  } //round
 
 
 } //end of class
