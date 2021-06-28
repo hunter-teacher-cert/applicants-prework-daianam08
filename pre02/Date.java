851751daed0f14ae7f6dc0bd9bca1c8906f61228
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-06-28 18:39:11 +0000

    made simple the variables

--- pre02/Date.java
+++ pre02/Date.java
@@ -3,14 +3,10 @@ import java.util.*;
 
 public class Date { 
     public static void main(String[] args) {
-String day;
-day= "Tuesday";
-int date;
-date = 8;
-String month;
-month = "June";
-int year;
-year = 2021;
+String day="Tuesday";
+int date=8;
+String month="June";
+int year=2021;
 System.out.println("American format:");
 System.out.println(day + ", "+ month +" "+ date+" "+ ", "+ year);
 System.out.println("European format:");
